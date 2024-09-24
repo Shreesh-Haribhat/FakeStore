60bf2fbec68e23cbdaa9cc001d6427c6e00fb58b
Shreesh Haribhat <shreeshscaler@gmail.com>
2024-09-25 04:52:24 +0530

    Croc

--- src/main/java/com/shreesh/fakestore/Config/ApplicationConfiguration.java
+++ src/main/java/com/shreesh/fakestore/Config/ApplicationConfiguration.java
@@ -3,7 +3,6 @@ package com.shreesh.fakestore.Config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
